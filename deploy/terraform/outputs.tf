6a86bbeecae65b3ffb879f90a23dfc602e0d9f50
Oleksii <ole4ryb@gmail.com>
2023-06-27 10:34:23 +0100

    tf state outputs

--- deploy/terraform/outputs.tf
+++ deploy/terraform/outputs.tf
@@ -4,3 +4,13 @@ output "web_public_dns" {
   value       = aws_lb.nia_gp2gp_elb.dns_name
   depends_on  = [aws_lb.nia_gp2gp_elb]
 }
+
+output "s3_state_bucket_arn" {
+  value = aws_s3_bucket.ps_adaptors_tf_state.arn
+  description = "The ARN of S3 state bucket"
+}
+
+output "dynamodb_state_table_name" {
+  value = aws_dynamodb_table.ps_adaptors_tf_locks.name
+  description = "The name of DynamoDB state table"
+}
