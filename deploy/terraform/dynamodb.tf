ca3cbd03ddedd0e72b5dbd7673cd59a9beee0b6c
Oleksii <ole4ryb@gmail.com>
2023-06-28 10:39:58 +0100

    formatting

--- deploy/terraform/dynamodb.tf
+++ deploy/terraform/dynamodb.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "mhs_state_table" {
-  name = "mhs-state"
-  hash_key = "key"
+  name         = "mhs-state"
+  hash_key     = "key"
   billing_mode = "PAY_PER_REQUEST"
 
   attribute {
@@ -10,8 +10,8 @@ resource "aws_dynamodb_table" "mhs_state_table" {
 }
 
 resource "aws_dynamodb_table" "mhs_sync_async_table" {
-  name = "mhs-sync-async-state"
-  hash_key = "key"
+  name         = "mhs-sync-async-state"
+  hash_key     = "key"
   billing_mode = "PAY_PER_REQUEST"
 
   attribute {
