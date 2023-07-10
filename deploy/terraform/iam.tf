22fb60b76c5288ba98835b6d0a4e69f8c9673913
Martin Wheeler <martin.wheeler@madetech.com>
2023-07-10 14:53:14 +0100

    NIAD-2747 Update iam.tf to use iam_policy_document data source

--- deploy/terraform/iam.tf
+++ deploy/terraform/iam.tf
@@ -1,135 +1,49 @@
 resource "aws_iam_role" "adaptor_execution" {
   name = "gp2gp_adaptor_execution"
-  assume_role_policy = jsonencode({
-    Version = var.iam_policy_version
-    Statement = var.iam_statement_assume_role
-  })
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "aws_iam_role_policy" "adaptor_execution_dyanmodb" {
+resource "aws_iam_role_policy" "adaptor_execution_dynamodb" {
   role = aws_iam_role.adaptor_execution.id
-  policy = jsonencode({
-    Version = var.iam_policy_version
-    Statement = [
-      {
-        Action = [
-          "dynamodb:*"
-        ]
-        Effect   = var.iam_role_policy_effect_allow
-        Resource = var.iam_role_policy_resource_all
-      },
-    ]
-  })
+  policy = data.aws_iam_policy_document.dynamodb.json
 }
 
 resource "aws_iam_role_policy" "adaptor_execution_s3" {
   role = aws_iam_role.adaptor_execution.id
-  policy = jsonencode({
-    Version = var.iam_policy_version
-    Statement = [
-      {
-        Action = [
-          "s3:*"
-        ]
-        Effect   = var.iam_role_policy_effect_allow
-        Resource = var.iam_role_policy_resource_all
-      },
-    ]
-  })
+  policy = data.aws_iam_policy_document.s3.json
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "gp2gp_ps_ecs_execution"
-  assume_role_policy = jsonencode({
-    Version = var.iam_policy_version
-    Statement = var.iam_statement_assume_role
-  })
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_iam_role_policy" "ecs_task_execution_role_policy" {
   name = "gp2gp_ps_ecs_execution_policy"
   role = aws_iam_role.ecs_task_execution_role.id
-  policy = jsonencode({
-    Version = var.iam_policy_version
-    Statement = [
-      {
-        Action = [
-          "ssm:GetParametersByPath",
-          "ssm:GetParameters",
-          "ssm:GetParameter"
-        ]
-        Effect   = var.iam_role_policy_effect_allow
-        Resource = var.iam_role_policy_resource_all
-      },
-    ]
-  })
+  policy = data.aws_iam_policy_document.ecs_task_execution
 }
 
 resource "aws_iam_role_policy" "ecs_logging_role_policy" {
   name = "gp2gp_ps_ecs_logging_policy"
   role = aws_iam_role.ecs_task_execution_role.id
-  policy = jsonencode({
-    Version = var.iam_policy_version
-    Statement = [
-      {
-        Action = [
-          "logs:*"
-        ]
-        Effect = var.iam_role_policy_effect_allow
-        Resource = var.iam_role_policy_resource_all
-      },
-    ]
-  })
+  policy = data.aws_iam_policy_document.ecs_logging.json
 }
 
 resource "aws_iam_role_policy" "mq_permissions" {
   name = "mq_permissions_policy"
   role = aws_iam_role.ecs_task_execution_role.id
-  policy = jsonencode({
-    Version = var.iam_policy_version
-    Statement = [
-      {
-        Action = [
-          "mq:*"
-        ]
-        Effect = var.iam_role_policy_effect_allow
-        Resource = var.iam_role_policy_resource_all
-      },
-    ]
-  })
+  policy = data.aws_iam_policy_document.mq_permissions.json
 }
 
 resource "aws_iam_role_policy" "secret_store_permissions" {
   name = "secret_store_permissions_policy"
   role = aws_iam_role.ecs_task_execution_role.id
-  policy = jsonencode({
-    Version = var.iam_policy_version
-    Statement = [
-      {
-        Action = [
-          "secretsmanager:*",
-          "ssm:*"
-        ]
-        Effect = var.iam_role_policy_effect_allow
-        Resource = var.iam_role_policy_resource_all
-      },
-    ]
-  })
+  policy = data.aws_iam_policy_document.secret_store_permissions.json
 }
 
 resource "aws_iam_role_policy" "s3_permissions" {
   name = "s3_policy"
   role = aws_iam_role.ecs_task_execution_role.id
-  policy = jsonencode({
-    Version = var.iam_policy_version
-    Statement = [
-      {
-        Action = [
-          "s3:*"
-        ]
-        Effect = var.iam_role_policy_effect_allow
-        Resource = var.iam_role_policy_resource_all
-      },
-    ]
-  })
+  policy = data.aws_iam_policy_document.s3.json
 }
