df6cbece2e86f4e09b2a79ae3fb63ad5ed9a28f7
azam145 <azam145@gmail.com>
2024-04-09 11:29:18 +0100

    Removed state resources from terraform.

--- deploy/terraform/outputs.tf
+++ deploy/terraform/outputs.tf
@@ -5,16 +5,6 @@ output "web_public_dns" {
   depends_on  = [aws_lb.nia_gp2gp_elb]
 }
 
-output "s3_state_bucket_arn" {
-  value       = aws_s3_bucket.ps_adaptors_tf_state.arn
-  description = "The ARN of S3 state bucket"
-}
-
-output "dynamodb_state_table_name" {
-  value       = aws_dynamodb_table.ps_adaptors_tf_locks.name
-  description = "The name of DynamoDB state table"
-}
-
 output "amq_endpoint" {
   value = aws_mq_broker.gp2gp_ps_broker.instances.0.endpoints.1
 }
